40738efdd0df9148d4783290c0708775200eac7f
mariamercedes797 <juanitabolanos8@gmail.com>
2024-09-28 21:15:42 -0500

    completamos el archivo variables

--- variables.py
+++ variables.py
@@ -7,3 +7,27 @@ nombre = "Mercedes"
 nombre =  "Mechys"
 #Podemos concluir que las variables son modificables
 print(nombre)
+numero = 10 #primero ejecuta esta linea de código y veras que se imprime el 10
+#numero = 10+1 #luego ejecuta esta línea de código y veras que se imprime el 11
+numero += 5 #de esta manera le sumamos 5 a la variable número
+print(numero)
+numero -= 5   #de esta manera le restamos 5 a la variable número
+print(numero)
+
+nombre = "Maria Mercedes Benavides"
+bienvenida = "Hola " + nombre + " Feliz Noche"
+print(bienvenida)
+bienvenida = f"Hola {nombre} Feliz Noche"
+print(bienvenida)
+#del bienvenida #borramos la variable, es decir la variable  que ya no esta declarada
+#print(bienvenida)  
+
+nombre = True
+bienvenida = f"Hola {nombre} feliz noche"
+print ("ola" in bienvenida)       
+
+print("Betty" not in bienvenida)
+print("Hola" not in bienvenida)
+
+nombreCompletoDeTuProfe ="Jheyson Galvis" #CamelCase sirve para definir variables
+nombre_completp_de_tu_profe "Jheyson Galvis" #Snake Case este es el recomendado en Python segun la documentacion
