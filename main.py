f9c64549bfc91f5ed8c067155a3a3c4cf2140a38
mariamercedes797 <juanitabolanos8@gmail.com>
2024-09-30 18:13:36 -0500

    Se abordaron generalidades de Python

---
+++ main.py
@@ -0,0 +1,24 @@
+name = "María"                       
+last_name = "Benavides"
+print(type(name))
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[-1])
+print(5* name)
+print(len(name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
+x = 10
+y = 5.678
+z = 1.2E6
+print(type(x))
+print(type(y))
+print(z)
+is_true = True
+is_false = False
+print(type(is_true))
+print(type(is_false))
